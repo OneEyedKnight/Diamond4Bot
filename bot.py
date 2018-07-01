34b69d93813483d77a1bf747fb177406c6cd7698
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-07-01 09:36:05 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,6 +26,26 @@ async def suggest(ctx,*, string):
     await client.say("Added! The suggestion will be reviewed soon...")
 
 @client.command(pass_context=True)
+async def test(self, message):
+        if message.content.startswith('$guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send('Sorry, you took too long it was {}.'.format(answer))
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send('Oops. It is actually {}.'.format(answer))
+
+@client.command(pass_context=True)
 async def power(ctx):
     power = open('power.txt').read().splitlines()
     power2 = random.choice(power)
