605d2721a2a6517a554e9b7b95be7fe5c84949ae
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-07-26 20:42:51 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,12 +170,12 @@ async def moti(ctx):
 async def love(ctx):
     love = random.choice([x for x in ctx.message.server.members if not x.bot])
     love2 = random.choice([x for x in ctx.message.server.members if not x.bot])  
-    years = random.randint(0, 100)
+    years = random.randint(0, 50)
     months = random.randint(0, 12)
     days = random.randint(0,32)
     embed = discord.Embed(title='We have found a secret couple in the server!', description = '{}loved {} for {} years, {} months and {} days!'.format(love, love2, years, months, days))
+    embed.set_image(url=https://cdn.discordapp.com/attachments/385419071727992834/472017700110073876/download.jpg")
     await client.say(embed=embed)
-    
     
 @client.command(pass_context=True)
 async def reco(ctx):
