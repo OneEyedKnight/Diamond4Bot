03f14c74b09002ea8adf2cf15b5bc63f96e196a1
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-08-04 10:12:11 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -115,9 +115,9 @@ async def test(ctx, member: discord.Member = None):
     greet3 = await client.wait_for_message(content='ok m8 lets go')
     await client.say('ok lets dance u fat boi')
     await client.say('what are u gonna start off with')
-    if await client.wait_for_message('kick')
+    if await client.wait_for_message('kick'):
        await client.say('OW!!!!!')
-    elif await client.wait_for_message('punch')
+    elif await client.wait_for_message('punch'):
        await client.say('OWWWIEEE!!!!')
     
 
