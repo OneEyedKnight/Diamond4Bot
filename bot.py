8db404c33c6898510d898318b2b2ee9e94f8428e
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-08-18 18:04:57 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -88,9 +88,9 @@ async def diary(ctx):
     ContentsDiary2 = random.choice(ContentsDiary)
     AuthorOfDiary = random.choice([x for x in ctx.message.server.members if not x.bot])
     if ctx.message.author.id == "206027308149112832":
-        embed = discord.Embed(title='<@!206027308149112832> found {}"s diary!'.format(AuthorOfDiary), description = '"{}{}'.format(AuthorOfDiary, ContentsDiary2))
+        embed = discord.Embed(title='<@!206027308149112832> found {}"s diary!'.format(AuthorOfDiary), description = '"Dear Diary, I,{}, {}'.format(AuthorOfDiary, ContentsDiary2))
     else:
-        embed = discord.Embed(title='{} found {}"s diary!'.format(ctx.message.author.mention, AuthorOfDiary), description = '{}{}'.format(AuthorOfDiary, ContentsDiary2))
+        embed = discord.Embed(title='{} found {}"s diary!'.format(ctx.message.author.mention, AuthorOfDiary), description = '"Dear Diary, I,{}, {}"'.format(AuthorOfDiary, ContentsDiary2))
     await client.say(embed=embed)
     
     
