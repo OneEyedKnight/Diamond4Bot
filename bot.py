58bbf17bd13300a341b5ed196e159c888793417b
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-08-20 20:39:55 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,8 +72,7 @@ async def help(ctx):
     embed.add_field(name="!kill", value="Randomly kill someone with something!")
     embed.add_field(name="!!game", value="See what game you like to play the most.")
     embed.add_field(name="!!power", value="See what hidden powers you have!")
-    embed2 = await client.say(embed=embedkek)
-    await client.edit_message(help1, embed=embed2)
+    await client.edit_message(help1, embed.to_dict(embedkek)
     
     
 
