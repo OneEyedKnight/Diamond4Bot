ccb3d385c591c33f5d6d582f8eb986bf77237f52
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-08-20 17:30:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ async def reac(ctx):
         res = await client.wait_for_reaction(['👍', '👎'], message=msgtest)
         embed2 = discord.Embed(title="Embed1", description ="embed2")
         await client.edit_message(msgtest, embed2)
-        await client.say(embed=embed)
+        await client.say(embed2=embed2)
 
 @client.group(pass_context=True, invoke_without_command=True)
 async def yon(ctx):
