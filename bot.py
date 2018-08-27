11ff71070bae5027883c3196b55f1d145d462993
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-08-27 20:13:28 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,12 +58,12 @@ async def cd(ctx):
 async def mute(ctx, member: discord.Member):
      if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
         role = discord.utils.get(member.server.roles, name='Muted')
-        await bot.add_roles(member, role)
+        await client.add_roles(member, role)
         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
-        await bot.say(embed=embed)
+        await client.say(embed=embed)
      else:
         embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
-        await bot.say(embed=embed)
+        await client.say(embed=embed)
         
 @client.command(pass_context=True)
 async def help(ctx):
