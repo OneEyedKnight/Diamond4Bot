9535dfb243d8e3fe96c0c307635cee58d83bdbd5
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-08-31 13:07:58 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,9 +29,9 @@ async def power(ctx):
         
 @client.command(pass_context=True)
 @commands.cooldown(1, 30, commands.BucketType.user)
-@commands.CommandOnCooldown(cooldown,retry_after)
 async def cooldown(ctx):
-    cooldown = await client.say("Ok. See if this has cooldown now.")
+    await client.say("Ok. See if this has cooldown now.")
+    await client.on_command_error("Test")
     
     
                     
