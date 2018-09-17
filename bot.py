d1a91dadf316360e998d7602285e13496dbb68ed
Diamond4luck <34939746+Diamond4luck@users.noreply.github.com>
2018-09-17 15:18:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ async def casino(ctx):
             await client.say("It was bigger than 50. You won!")
         else:
             await client.say("It was bigger than 50. You lost.")
-   else:
+   elif casinonumber5 <= 50:
         if 'Smaller' in message.content:
             await client.say("It was smaller than 50. You lost.")
         else:
